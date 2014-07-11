a62965cde7eb6ee583b1ed87b125daaf79ff987c
Marta Milakovic <marta.milakovic@gmail.com>
2014-07-11 23:36:07 +0200

    Adding automake files

---
+++ autogen.sh
@@ -0,0 +1,4 @@
+autoreconf --force --install --verbose || exit 1
+# You can optionally perform other tasks here such as cleaning up generated
+# files that are no longer needed, or running `./configure && make` if that is
+# convenient for your users
