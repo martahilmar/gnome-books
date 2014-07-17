983e62d0d0754c3edf9996170fa62b8124cd1742
Marta Milakovic <marta.milakovic@gmail.com>
2014-07-17 10:45:24 +0200

    Changes in autotools

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,23 @@
-autoreconf --force --install --verbose || exit 1
-# You can optionally perform other tasks here such as cleaning up generated
-# files that are no longer needed, or running `./configure && make` if that is
-# convenient for your users
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="gnome-books"
+
+(test -f $srcdir/gnome-books.js) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME git"
+    exit 1
+}
+
+git submodule update --init --recursive
+
+REQUIRED_AUTOCONF_VERSION=2.59
+REQUIRED_AUTOMAKE_VERSION=1.9
+REQUIRED_INTLTOOL_VERSION=0.40.0
+REQUIRED_PKG_CONFIG_VERSION=0.22
+. gnome-autogen.sh
