0c25f1dd02f58686edc548cd3e428bbcb4e8fac9
Marta Milakovic <marta.milakovic@gmail.com>
2014-08-15 01:30:56 +0200

    Build tools changes

---
+++ src/config.js
@@ -0,0 +1,3 @@
+const PACKAGE_NAME = 'Books';
+const PACKAGE_VERSION = '1.0.0';
+const GETTEXT_PACKAGE = '';
---
+++ src/path.js
@@ -0,0 +1,4 @@
+let LOCALE_DIR = "/usr/local/share/locale";
+let STYLE_DIR = "/usr/local/share/books/style/";
+let ICONS_DIR = "/usr/local/share/books/icons/";
+let RESOURCE_DIR = "/usr/local/share/books";
